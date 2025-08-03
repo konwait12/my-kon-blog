4bae8af4a15a2883e74bb3a158e2518f31492154
konwait12 <3043520499@qq.com>
2025-08-03 15:56:48 +0800

    feat: 添加新文章《新文章标题》

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -7,11 +7,14 @@ export const DEFAULT_THEME = AUTO_MODE;
 
 // Banner height unit: vh
 export const BANNER_HEIGHT = 35;
-export const BANNER_HEIGHT_EXTEND = 30;
+export const BANNER_HEIGHT_EXTEND = 65;
 export const BANNER_HEIGHT_HOME = BANNER_HEIGHT + BANNER_HEIGHT_EXTEND;
 
+//export const BANNER_HEIGHT_HOME = 100; // 首页横幅全屏
+//export const BANNER_HEIGHT = 100; // 其他页面横幅全屏（如有需要）
+
 // The height the main panel overlaps the banner, unit: rem
-export const MAIN_PANEL_OVERLAPS_BANNER_HEIGHT = 3.5;
+export const MAIN_PANEL_OVERLAPS_BANNER_HEIGHT = 20;
 
 // Page width: rem
 export const PAGE_WIDTH = 75;
